f1ebbc4609f9635fe11b309225154fb4812f8141
Tanishq Patil <59779592+tpatil17@users.noreply.github.com>
2026-09-17 13:24:39 -0700

    Implement Digit classifier (Training 4/4)

---
+++ foundations/digit_classifier.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+from torchtyping import TensorType
+
+class Solution(nn.Module):
+    def __init__(self):
+        super().__init__()
+        torch.manual_seed(0)
+        # Architecture: Linear(784, 512) -> ReLU -> Dropout(0.2) -> Linear(512, 10) -> Sigmoid
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(512, 10),
+            nn.Sigmoid()
+        )
+
+    def forward(self, images: TensorType[float]) -> TensorType[float]:
+        torch.manual_seed(0)
+
+        return self.linear_relu_stack(images)
+        
